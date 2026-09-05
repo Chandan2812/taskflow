6d7b76d357072896e9d4bb631cdbec286e1abe3c
chandan2812 <chandanpms@gmail.com>
2026-09-05 13:08:59 +0530

    server .env

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const { initializeSocket } = require("./socket");
 const app = require("./app");
 const { connectRedis } = require("./redis");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 async function startServer() {
   try {
