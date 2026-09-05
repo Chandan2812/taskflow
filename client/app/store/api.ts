c6a2436a3f3a68ab5118cd26d86e9e9eddf0d4db
chandan2812 <chandanpms@gmail.com>
2026-09-05 13:29:14 +0530

    updated api.ts

--- client/app/store/api.ts
+++ client/app/store/api.ts
@@ -6,7 +6,7 @@ export const api = createApi({
   tagTypes: ["Project", "Task"],
 
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
 
     prepareHeaders: (headers, { getState }) => {
       const state = getState() as {
